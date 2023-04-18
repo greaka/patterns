9b74e1ea70c5652495e71ed6e58fd0e382ca4ea4
Greaka <git@greaka.de>
2023-04-18 21:22:17 +0200

    improve c api

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -3,11 +3,16 @@ use core::{slice, str::from_utf8};
 use patterns::Pattern;
 
 /// # Safety
-/// `len` must be a valid length of `pat`. On success, `res` will not be null.
-/// There is no guarantee about the layout of `res` and it should be considered
-/// opaque. The buffer behind `res` must be of size 256 bytes
+/// `len` must be a valid length of `pat`. On success, the content of `res` will
+/// not be null. There is no guarantee about the layout of `res` and it should
+/// be considered opaque. The buffer behind `res` must be of size 256 bytes and
+/// needs to be aligned to [`patterns::BYTES`] bytes! By default 64.
+/// `pat` needs to be valid UTF-8.
 #[no_mangle]
 pub unsafe extern "C" fn parse_pattern(pat: *const u8, len: usize, res: *mut Pattern) {
+    if pat.is_null() || res.is_null() {
+        return;
+    }
     if let Some(pattern) = from_utf8(slice::from_raw_parts(pat, len))
         .ok()
         .and_then(|x| x.parse().ok())
@@ -17,28 +22,36 @@ pub unsafe extern "C" fn parse_pattern(pat: *const u8, len: usize, res: *mut Pat
 }
 
 /// # Safety
-/// `pat` needs to be aligned to [`patterns::BYTES`] bytes! By default 64.
-/// `len` must be the number of bytes of `data`. `res` must hold at least
-/// `8 * number of results` bytes. The number required is unknown.
-/// On success, `*num_res` will not be null.
+/// [in] `pat` must be the same pointer that was filled by [`parse_pattern`].
+/// [in] `data` is the data to search through
+/// [in] `len` must be the number of bytes of `data`.
+/// [out] `res` will be filled with the result
+/// [in] `res_len` is the amount of results that fit into `res`
+/// [return] returns how many offsets were found
 #[no_mangle]
 pub unsafe extern "C" fn match_pattern(
     pat: *const Pattern,
     data: *const u8,
     len: usize,
     res: *mut usize,
-    num_res: *mut usize,
-) {
-    if pat.is_null() {
-        return;
+    res_len: usize,
+) -> usize {
+    if pat.is_null() || res.is_null() || data.is_null() {
+        return 0;
     }
     let data = slice::from_raw_parts(data, len);
+    let res = slice::from_raw_parts_mut(res, res_len);
     let pattern = &*pat;
-    let scan = pattern.matches(data);
-    for (index, found) in scan.enumerate() {
-        *res.add(index) = found;
-        *num_res = index + 1;
+    let mut scan = pattern.matches(data);
+    for (index, element) in res.iter_mut().enumerate() {
+        let Some(offset) = scan.next() else {
+            return index;
+        };
+
+        *element = offset;
     }
+
+    res_len
 }
 
 #[cfg(test)]
@@ -53,15 +66,15 @@ mod tests {
         data[len - 8..].fill(1);
         let mut res: Pattern = unsafe { core::mem::zeroed() };
         let mut results = [0usize; 1];
-        let mut num_results = 0usize;
+        let num_results = 1usize;
         unsafe {
             parse_pattern(pattern.as_bytes().as_ptr(), pattern.len(), &mut res as _);
             match_pattern(
                 &res as _,
                 data.as_ptr() as _,
                 data.len(),
-                &mut results as _,
-                &mut num_results as _,
+                results.as_mut_ptr(),
+                1,
             );
         }
         assert_eq!(num_results, 1);
