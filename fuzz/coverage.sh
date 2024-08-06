d374f10d9e4f73a6e0048d6e1ca3c44b41f251fb
Mamvish <126025727+Mamvish@users.noreply.github.com>
2024-08-07 01:25:16 +0200

    add a cargo-fuzz differential fuzzer

---
+++ fuzz/coverage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash -e
+cd "$(dirname "$0")" || exit 1
+PROJROOT="$(cd .. && pwd)"
+cargo fuzz coverage -D diff
+rust-cov report --use-color --instr-profile=coverage/diff/coverage.profdata ./target/*/coverage/*/debug/diff -Xdemangler=rustfilt --show-functions --sources ../src/* | aha -b | sed 's#'"${PROJROOT}"/'##g' > coverage/diff/stats.html
+rust-cov show --format=html --instr-profile=coverage/diff/coverage.profdata ./target/*/coverage/*/debug/diff -Xdemangler=rustfilt --show-instantiations=false --sources ../src/* | sed 's#'"${PROJROOT}"/'##g' > coverage/diff/files.html
+rust-cov show --format=html --instr-profile=coverage/diff/coverage.profdata ./target/*/coverage/*/debug/diff -Xdemangler=rustfilt --show-instantiations=true --sources ../src/* | sed 's#'"${PROJROOT}"/'##g' > coverage/diff/instantiations.html
